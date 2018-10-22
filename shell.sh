4b12ce0b731cb7c59e35ee6cec987c4ce07db480
ziauislammaju <ziaulislammaju@gmail.com>
2018-10-22 19:26:37 +0300

    shell.sh week1

---
+++ shell.sh
@@ -0,0 +1,23 @@
+ziaul@ziaul-ayaLIFEBOOK-S752:~$cd Desktop
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop$ mkdir cli.sha
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop$ cd cli.sha
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$touch shell.sh
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$ touch blank.txt
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$ touch greeting.txt
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$ echo "HELLO HELLO HELLO HELLO HELLo" >greeting.txt 
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$ cp greeting.txt 1.txt
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$ cp greeting.txt 2.txt 
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$ cp greeting.txt 3.txt
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$ cp greeting.txt 3.txt
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$ cp greeting.txt 4.txt
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$ cp greeting.txt 5.txt 
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$ echo "cat" > pets.txt
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$ echo " dog" >> pets.txt
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$ echo " hamster" >> pets.txt
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$ echo "cat " > command.txt
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$ echo "ls" >> commands.txt 
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$ echo " pwd" >> commands.txt
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$ cat pets.txt commands.txt |sort | uniq > lovelyCommands.txtl
+ziaul@ziaul-ayaLIFEBOOK-S752:~/Desktop/cli.sha$ 
+
+
